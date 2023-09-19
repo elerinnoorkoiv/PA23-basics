d6a4c70a764a51fe4736ac5639d087e3ccf70efc
Elerin Noorkoiv <elerin.noorkoiv@voco.ee>
2023-09-19 13:55:42 +0300

    numbers.js

---
+++ numbers.js
@@ -0,0 +1,31 @@
+ //data types
+ //string
+ let firstname = 'Elerin'
+ let lastname = 'Noorkoiv'
+
+ console.log(firstname + ' ' + lastname)
+console.log(typeof firstname)
+
+// Numbers 
+let goodMark = 5
+console.log(typeof goodMark)
+
+let percent = 50.3
+console.log(typeof percent)
+
+// Boolean
+let isToday = true
+console.log(typeof isToday)
+
+// type conversions
+console.log(String(5+5))
+console.log(String(5.5) + String(5.5))
+
+// boolean to string
+console.log(typeof String(true))
+
+// String to number
+console.log(Number('666'))
+console.log(Number('66.6'))
+console.log(parseInt(100.3))
+console.log(parseFloat(100.3))
