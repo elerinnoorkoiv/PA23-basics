93695556054d4971fa90f1271fc79e8e29cd757b
Elerin Noorkoiv <elerin.noorkoiv@voco.ee>
2023-09-19 12:03:40 +0300

    js variables - declare, assign an use

---
+++ variables.js
@@ -0,0 +1,18 @@
+//variables
+//keyword variablename = value;
+//let
+
+let programmingLanguage = 'Java Script'
+console.log(programmingLanguage)
+
+// assignment - save new value into the variable
+programmingLanguage = 'PHP'
+console.log(programmingLanguage)
+
+let result
+console.log(result)
+
+//const
+const group = 'VS23'
+// group = 'PG23' - not allowed
+console.log(group)
