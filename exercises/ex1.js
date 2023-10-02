64e2ad00b20b16085c97464129b299cc4701bbd6
Elerin Noorkoiv <elerin.noorkoiv@voco.ee>
2023-10-02 16:22:02 +0300

    PA23-basics

---
+++ exercises/ex1.js
@@ -0,0 +1,9 @@
+console.log('Tere, maailm!')
+
+let aasta  2016
+let president = 'Kersti Kaljulaid'
+let lause Keskosa = '. aastal valiti Eesti presidendiks '
+
+let lause = aasta + lauseKeskosa + president
+
+console.log(lause)
